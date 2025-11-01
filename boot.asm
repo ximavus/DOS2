d3158899745c4096fb62698ac16b1de3027a2712
ximavus <ximavus27@gmail.com>
2025-11-01 12:49:18 +0200

    Update boot.asm
    Added cls command

--- boot.asm
+++ boot.asm
@@ -14,8 +14,7 @@ mainloop:
    mov di, buffer
    call get_string
  
-   mov si, buffer
-   cmp byte [si], 0
+   cmp byte [buffer], 0
    je mainloop
  
    mov si, buffer
@@ -37,10 +36,20 @@ mainloop:
    call isecho ; The code for calling the echo command
    jc .echo
 
+   mov si, buffer
+   mov di, cmd_cls
+   call strcmp
+   jc .cls
+
    mov si, badcommand
    call print_string ; Not a command!
    jmp mainloop
  
+ .cls:
+    mov ah, 0x0
+    mov al, 0x03
+    int 0x10
+    jmp mainloop
  .help:
    mov si, msg_help
    call print_string
@@ -65,16 +74,17 @@ mainloop:
    int 0x10
    jmp mainloop
  
-welcome db "MiniCMD v0.2!", 0x0D, 0x0A, 0
+welcome db "MiniCMD v0.3!", 0x0D, 0x0A, 0
 badcommand db "!", 0x0D, 0x0A, 0
 prompt db "> ", 0
+cmd_cls db "cls", 0
 cmd_help db "help", 0
 cmd_desc db "desc", 0
 cmd_quit db "quit", 0
-msg_help db "help, desc (prints desc), quit (turn off), echo (print str)", 0x0D, 0x0A, 0
+msg_help db "help, desc, quit, echo, cls", 0x0D, 0x0A, 0
 msg_desc db "DOS-like OS", 0x0D, 0x0A, 0
-buffer times 64 db 0
- 
+buffer times 32 db 0
+
 print_string:
    lodsb
    or al, al
@@ -118,13 +128,13 @@ get_string:
  
    mov ah, 0x0E
    mov al, 0x08
-   int 10h
+   int 0x10
  
    mov al, ' '
-   int 10h
+   int 0x10
  
    mov al, 0x08
-   int 10h
+   int 0x10
  
    jmp .loop
  
